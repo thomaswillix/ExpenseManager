ca95aa51efb35f56609ea1370bec538e2b9dea49
Thomas Freitas <tfreitas@iteggo.com>
2024-12-04 17:01:03 +0100

    Sign up logic

--- app/src/main/java/com/example/expensemanager/SignupActivity.kt
+++ app/src/main/java/com/example/expensemanager/SignupActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.expensemanager.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.userProfileChangeRequest
 
 class SignupActivity : AppCompatActivity() {
 
@@ -17,6 +19,11 @@ class SignupActivity : AppCompatActivity() {
     //Binding
     private lateinit var binding: ActivitySignupBinding
     private lateinit var progressDialog : AlertDialog
+    private lateinit var email : String
+    private lateinit var password: String
+    private lateinit var confirmPassword: String
+    private lateinit var username : String
+    val noWhiteSpace = "\\A[.\\- \\w]{4,20}\\z"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,25 +43,19 @@ class SignupActivity : AppCompatActivity() {
     private fun registration(){
         binding.signupBtn.setOnClickListener{
             progressDialog.show()
-            val email = binding.signupEmail.text.toString()
-            val password = binding.signupPassword.text.toString()
-            val confirmPassword = binding.signUpConfirm.text.toString()
-            if (TextUtils.isEmpty(email)){
-                progressDialog.dismiss()
-                binding.signupEmail.setError("Email is required")
-            }
-            if (TextUtils.isEmpty(password)){
-                progressDialog.dismiss()
-                binding.signupPassword.setError("Password is required")
-            }
-            if (TextUtils.isEmpty(confirmPassword)){
-                progressDialog.dismiss()
-                binding.signUpConfirm.setError("Password confirmation is required")
-            }
-            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+            email = binding.signupEmail.text.toString()
+            password = binding.signupPassword.text.toString()
+            confirmPassword = binding.signUpConfirm.text.toString()
+            username = binding.signupUsername.text.toString()
+
+            if (validateSignIn()){
                 if (password == confirmPassword){
                     auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
                         if (it.isSuccessful){
+                            val profileUpdates = userProfileChangeRequest {
+                                displayName = username
+                            }
+                            FirebaseAuth.getInstance().currentUser!!.updateProfile(profileUpdates)
                             progressDialog.dismiss()
                             Toast.makeText(this, "Registration was completed successfully", Toast.LENGTH_SHORT).show()
                             val intent = Intent(this, LoginActivity::class.java)
@@ -76,5 +77,41 @@ class SignupActivity : AppCompatActivity() {
             startActivity(loginIntent)
             this.finish()
         }
+    }
+    private fun validateSignIn(): Boolean{
+        var isValid = true
+        if (TextUtils.isEmpty(username)) {
+            isValid = false
+            progressDialog.dismiss()
+            binding.signupUsername.setError("Username confirmation is required")
+        } else if (username.length < 4){
+            isValid = false
+            progressDialog.dismiss()
+            binding.signupUsername.setError("Username too short")
+        } else if (username.length > 15){
+            isValid = false
+            progressDialog.dismiss()
+            binding.signupUsername.setError("Username too long")
+        } else if(!username.matches(noWhiteSpace.toRegex())){
+            isValid = false
+            progressDialog.dismiss()
+            binding.signupUsername.setError("No whitespaces allowed")
+        }
+        if (TextUtils.isEmpty(email)){
+            isValid = false
+            progressDialog.dismiss()
+            binding.signupEmail.setError("Email is required")
+        }
+        if (TextUtils.isEmpty(password)){
+            isValid = false
+            progressDialog.dismiss()
+            binding.signupPassword.setError("Password is required")
+        }
+        if (TextUtils.isEmpty(confirmPassword)){
+            isValid = false
+            progressDialog.dismiss()
+            binding.signUpConfirm.setError("Password confirmation is required")
+        }
+        return isValid
     }
 }
