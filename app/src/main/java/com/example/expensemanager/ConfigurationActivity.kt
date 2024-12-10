02c82a6d4ce3f06187fefd3057f9dc81d96b2d8a
Thomas Freitas <tfreitas@iteggo.com>
2024-12-10 13:59:00 +0100

    Configuration activity now has a logout button (now it works)

--- app/src/main/java/com/example/expensemanager/ConfigurationActivity.kt
+++ app/src/main/java/com/example/expensemanager/ConfigurationActivity.kt
@@ -1,11 +1,17 @@
 package com.example.expensemanager
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class ConfigurationActivity : AppCompatActivity() {
     private var nightMode : Boolean = false
@@ -28,12 +34,47 @@ class ConfigurationActivity : AppCompatActivity() {
         editor = sharedPreferences.edit()
         nightMode = sharedPreferences.getBoolean("night",false)
 
-        if (nightMode){
+        /*if (nightMode){
             switch.isChecked = true
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
-        switch.setOnClickListener { myThemes() }
+        switch.setOnClickListener { myThemes() }*/
+        val logout : Button = findViewById(R.id.logoutButton)
+        logout.setOnClickListener{
+            showDialog()
+        }
+
     }
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(R.layout.dialog_logout, null)
+        builder.setView(view)
+        val dialog = builder.create()
+        view.findViewById<Button>(R.id.btnReturn).setOnClickListener {
+            dialog.dismiss()
+        }
+        view.findViewById<Button>(R.id.btnLogout).setOnClickListener {
+            dialog.dismiss()
+            logoutUser()
+        }
+        if (dialog.window != null){
+            dialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+        }
+        dialog.show()
+    }
+
+    private fun logoutUser() {
+        FirebaseAuth.getInstance().signOut()
+        Toast.makeText(this, "Logged out successfully", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+
+        finish()
+    }
+
 
 
     private fun myThemes() {
